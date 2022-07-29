9881caf84eb705defa1f3e3acf05d8758c6999e8
Nesatech <houeesteve@gmail.com>
2022-07-29 15:29:45 +0200

    Initial commit

---
+++ src/main/java/com/test/api_0822_sh/Api0822ShApplication.java
@@ -0,0 +1,13 @@
+package com.test.api_0822_sh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api0822ShApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Api0822ShApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/api_0822_sh/Api0822ShApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.api_0822_sh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api0822ShApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
