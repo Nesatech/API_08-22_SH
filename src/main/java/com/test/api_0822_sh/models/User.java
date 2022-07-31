11ab954eb726c22394830b9b45ab159e7b5195b4
Nesatech <houeesteve@gmail.com>
2022-07-31 22:05:25 +0200

    [DEV] Update user model to add persistence, custom constructor and annotations needed for JPA

--- src/main/java/com/test/api_0822_sh/models/User.java
+++ src/main/java/com/test/api_0822_sh/models/User.java
@@ -1,21 +1,54 @@
 package com.test.api_0822_sh.models;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.Objects;
 
-@Data
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "`user`") // prevent H2 exception because of 'user' reserved word
 public class User {
 
     @Id
     @GeneratedValue
     private Long id;
+    @NonNull
     private String name;
+    @NonNull
     private LocalDate birthday;
+    @NonNull
     private String country;
     private String phoneNumber;
     private char gender;
 
+    public User(String name, LocalDate birthday, String country, String phoneNumber, char gender) {
+        this.name = name;
+        this.birthday = birthday;
+        this.country = country;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
