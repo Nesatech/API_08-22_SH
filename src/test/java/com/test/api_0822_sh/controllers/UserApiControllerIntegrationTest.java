3561d783227276f42521df9a1a56542fb2925ead
Nesatech <houeesteve@gmail.com>
2022-08-05 15:00:18 +0200

    [DEV] Delete MockBean repository

--- src/test/java/com/test/api_0822_sh/controllers/UserApiControllerIntegrationTest.java
+++ src/test/java/com/test/api_0822_sh/controllers/UserApiControllerIntegrationTest.java
@@ -10,8 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -19,12 +19,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -32,29 +29,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class UserApiControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     UserRepository userRepository;
 
     private List<User> users;
 
     @BeforeEach
-    void initUseCase() {
+    public void initUseCase() {
         users = Arrays.asList(
                 new User("Brigitte", LocalDate.of(1989, 11, 20), "France", "0235869742", 'F'),
                 new User("Serge", LocalDate.of(1979, 1, 26), "France", "0214758696", 'M')
         );
+        userRepository.save(users.get(0));
+        userRepository.save(users.get(1));
     }
 
     @DisplayName("FindAll_Success")
     @Test
     public void findAllUser_thenReturnJsonArray() throws Exception {
-        when(userRepository.findAll()).thenReturn(users);
-
         MvcResult mvcResult = mockMvc.perform(get("/users")
                         .contentType(MediaType.APPLICATION_JSON))
 
@@ -62,14 +60,14 @@ public class UserApiControllerIntegrationTest {
 
                 .andExpect(jsonPath("$", hasSize(2)))
 
-                .andExpect(jsonPath("$[0].id", is(users.get(0).getId())))
+                .andExpect(jsonPath("$[0].id", is(users.get(0).getId().intValue())))
                 .andExpect(jsonPath("$[0].name", is(users.get(0).getName())))
                 .andExpect(jsonPath("$[0].birthday", is(users.get(0).getBirthday().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")))))
                 .andExpect(jsonPath("$[0].country", is(users.get(0).getCountry())))
                 .andExpect(jsonPath("$[0].phoneNumber", is(users.get(0).getPhoneNumber())))
                 .andExpect(jsonPath("$[0].gender", is(users.get(0).getGender().toString())))
 
-                .andExpect(jsonPath("$[1].id", is(users.get(1).getId())))
+                .andExpect(jsonPath("$[1].id", is(users.get(1).getId().intValue())))
                 .andExpect(jsonPath("$[1].name", is(users.get(1).getName())))
                 .andExpect(jsonPath("$[1].birthday", is(users.get(1).getBirthday().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")))))
                 .andExpect(jsonPath("$[1].country", is(users.get(1).getCountry())))
@@ -84,14 +82,12 @@ public class UserApiControllerIntegrationTest {
     @DisplayName("FindById_Success")
     @Test
     public void findById_thenReturnJsonArray() throws Exception {
-        when(userRepository.findById(1L)).thenReturn(Optional.ofNullable(users.get(0)));
-
         MvcResult mvcResult = mockMvc.perform(get("/users/1")
                         .contentType(MediaType.APPLICATION_JSON))
 
                 .andExpect(status().isOk())
 
-                .andExpect(jsonPath("$.id", is(users.get(0).getId())))
+                .andExpect(jsonPath("$.id", is(users.get(0).getId().intValue())))
                 .andExpect(jsonPath("$.name", is(users.get(0).getName())))
                 .andExpect(jsonPath("$.birthday", is(users.get(0).getBirthday().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")))))
                 .andExpect(jsonPath("$.country", is(users.get(0).getCountry())))
@@ -105,7 +101,7 @@ public class UserApiControllerIntegrationTest {
     @DisplayName("FindById_Error")
     @Test
     public void getWrongId_whenFindById_then404() throws Exception {
-        mockMvc.perform(get("/users/3")
+        mockMvc.perform(get("/users/100")
                         .contentType(MediaType.APPLICATION_JSON))
 
                 .andExpect(status().isNotFound())
@@ -115,15 +111,13 @@ public class UserApiControllerIntegrationTest {
     @DisplayName("PostUser_Success")
     @Test
     public void saveUser_thenCreateUserStatus201() throws Exception {
-        when(userRepository.save(any(User.class))).thenReturn(users.get(0));
-
         MvcResult mvcResult = mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(users.get(0))))
 
                 .andExpect(status().isCreated())
 
-                .andExpect(jsonPath("$.id", is(users.get(0).getId())))
+                .andExpect(jsonPath("$.id", is(users.get(0).getId().intValue())))
                 .andExpect(jsonPath("$.name", is(users.get(0).getName())))
                 .andExpect(jsonPath("$.birthday", is(users.get(0).getBirthday().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")))))
                 .andExpect(jsonPath("$.country", is(users.get(0).getCountry())))
